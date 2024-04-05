68eefdea3772ee2d32890590ec496390b94d0f17
NusretOzates <m.nusret.ozates@gmail.com>
2024-04-05 04:21:57 +0300

    Migrate resnet to keras 3

--- tf_model_implementations/cv/resnet_v1.py
+++ tf_model_implementations/cv/resnet_v1.py
@@ -6,15 +6,14 @@ The model is based on the paper: https://arxiv.org/pdf/1512.03385.pdf
 It consists of stacked residual blocks.
 Each residual block has two 3x3 convolutions with batch normalization and ReLU.
 """
-
+import keras
 import tensorflow as tf
 from keras import layers
-from tensorflow import keras
 from enum import Enum
 
 
 def batch_normalized_conv2d(
-        x: tf.Tensor,
+        x: keras.KerasTensor,
         filter_size: int,
         kernel_size: int,
         stride_size: int,
@@ -28,7 +27,7 @@ def batch_normalized_conv2d(
 
 
 def residual_block(
-        x: tf.Tensor,
+        x: keras.KerasTensor,
         filter_size: int,
         stride_size: int,
         activation: str,
@@ -59,7 +58,7 @@ def residual_block(
 
 
 def residual_stack(
-        x: tf.Tensor, filter_size: int, block_count: int, stride_size: int, activation: str
+        x: keras.KerasTensor, filter_size: int, block_count: int, stride_size: int, activation: str
 ):
     x = residual_block(
         x=x,
@@ -75,7 +74,7 @@ def residual_stack(
     return x
 
 
-def resnet50(x: tf.Tensor, activation: str):
+def resnet50(x: keras.KerasTensor, activation: str):
     x = residual_stack(x, 64, 3, 1, activation)
     x = residual_stack(x, 128, 4, 2, activation)
     x = residual_stack(x, 256, 6, 2, activation)
@@ -83,7 +82,8 @@ def resnet50(x: tf.Tensor, activation: str):
 
     return x
 
-def resnet16(x: tf.Tensor, activation: str):
+
+def resnet16(x: keras.KerasTensor, activation: str):
     x = residual_stack(x, 64, 2, 1, activation)
     x = residual_stack(x, 128, 2, 2, activation)
     x = residual_stack(x, 256, 2, 2, activation)
@@ -91,7 +91,8 @@ def resnet16(x: tf.Tensor, activation: str):
 
     return x
 
-def resnet101(x: tf.Tensor, activation: str):
+
+def resnet101(x: keras.KerasTensor, activation: str):
     x = residual_stack(x, 64, 3, 1, activation)
     x = residual_stack(x, 128, 4, 2, activation)
     x = residual_stack(x, 256, 23, 2, activation)
@@ -99,13 +100,15 @@ def resnet101(x: tf.Tensor, activation: str):
 
     return x
 
-def resnet152(x: tf.Tensor, activation: str):
+
+def resnet152(x: keras.KerasTensor, activation: str):
     x = residual_stack(x, 64, 3, 1, activation)
     x = residual_stack(x, 128, 8, 2, activation)
     x = residual_stack(x, 256, 36, 2, activation)
     x = residual_stack(x, 512, 3, 2, activation)
 
     return x
+
 
 class ResnetEnum(Enum):
     resnet16 = 16
@@ -114,9 +117,8 @@ class ResnetEnum(Enum):
     resnet152 = 152
 
 
-
-# TODO: Make it more generic and add more options such as resnet50, resnet101, etc.
-def ResNet(rescale: bool, input_shape, batch_count, activations: str = "relu", pooling='avg', resnet_version=ResnetEnum.resnet50):
+def ResNet(rescale: bool, input_shape, batch_count, activations: str = "relu", pooling='avg',
+           resnet_version=ResnetEnum.resnet50):
     inputs = layers.Input(input_shape, batch_count)
     if rescale:
         x = layers.Rescaling(scale=1.0 / 127.5, offset=-1)(inputs)
